ad0449f70b7f370bbf2674158c5572a68a096e95
liuyao <670652018@qq.com>
2018-09-21 22:43:02 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,28 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
   parserOptions: {
-    parser: 'babel-eslint',
     ecmaVersion: 6,
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true
     }
   },
-  env: {
-    browser: true,
+  settings: {
+    react: {
+      version: '16.5.2'
+    }
   },
-  extends: ['plugin:react/recommended', 'airbnb-base'],
+  extends: ['airbnb-base', 'plugin:react/recommended'],
   plugins: [
     'react'
   ],
-  // check if imports actually resolve
-  settings: {
-    react: {
-      version: '16.5.1',
-    },
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  // add your custom rules here
   rules: {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/no-named-as-default-member': 0,
-    'import/no-named-as-default': 0,
-    'class-methods-use-this': 0,
+    'class-methods-use-this': 0
   }
-}
+};
