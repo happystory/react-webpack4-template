f5f2c20ddec0082040708fbe0860df209f8bbfa7
liuyao <670652018@qq.com>
2018-09-22 12:56:13 +0800

    show ip

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,15 +1,22 @@
 const path = require('path');
+const os = require('os');
+const dns = require('dns');
+const { promisify } = require('util');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const portfinder = require('portfinder');
+const chalk = require('chalk');
 const baseWebpackConfig = require('./webpack.base.conf');
 
-const HOST = process.env.HOST || '0.0.0.0';
+const lookup = promisify(dns.lookup);
+
+const HOST = '0.0.0.0';
 const PORT = (process.env.PORT && Number(process.env.PORT)) || 8080;
 
-module.exports = merge(baseWebpackConfig, {
+const devWebpackConfig =  merge(baseWebpackConfig, {
   mode: 'development',
   module: {
     rules: [
@@ -98,16 +105,37 @@ module.exports = merge(baseWebpackConfig, {
         ignore: ['.*', 'index.html']
       }
     ]),
-    new FriendlyErrorsPlugin({
-      compilationSuccessInfo: {
-        messages: [`Your application is running here: http://${HOST}:${PORT}`],
-      },
-      onErrors: undefined,
-      clearConsole: true,
-    }),
   ],
   optimization: {
     namedModules: true,
     noEmitOnErrors: true,
   },
 });
+
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = PORT;
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err);
+    } else {
+      devWebpackConfig.devServer.port = port;
+
+      lookup(os.hostname()).then(res => {
+        const ip = res.address;
+
+        devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+          compilationSuccessInfo: {
+            messages: [chalk`App running at:\n  - Local:   {cyan http://localhost:{bold ${port}/}}\n  - Network: {cyan http://${ip}:{bold ${port}/}}`],
+          },
+          onErrors: undefined,
+          clearConsole: true,
+        }));
+
+        resolve(devWebpackConfig);
+      }).catch(err => {
+        reject(err);
+      });
+    }
+  })
+})
